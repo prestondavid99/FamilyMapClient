f21416be2288d63f4661485012609b3c99161c4d
Preston Thomas <preston@gmail.com>
2022-03-22 18:35:03 -0600

    First Legitimate Commit

--- app/src/main/java/com/example/familymapclient/ServerProxy.java
+++ app/src/main/java/com/example/familymapclient/ServerProxy.java
@@ -1,7 +1,12 @@
 package com.example.familymapclient;
 import com.google.gson.Gson;
+
+import requestresult.EventResult;
 import requestresult.LoginRequest;
 import requestresult.LoginResult;
+import requestresult.PersonResult;
+import requestresult.RegisterRequest;
+import requestresult.RegisterResult;
 
 
 import java.io.*;
@@ -25,17 +30,11 @@ public class ServerProxy {
             // Indicate that this request will contain an HTTP request body
             http.setDoOutput(true);
 
-
-            // Specify that we would like to receive the server's response in JSON
-            // format by putting an HTTP "Accept" header on the request (this is not
-            // necessary because our server only returns JSON responses, but it
-            // provides one more example of how to add a header to an HTTP request).
-            http.addRequestProperty("Accept", "application/json");
-
             // Connect to the server and send the HTTP request
             http.connect();
 
-            String reqData = http.getRequestMethod(); // TODO : Is this correct ? (lines 60-69)
+            Gson gson = new Gson();
+            String reqData = gson.toJson(r);
             // Get the output stream containing the HTTP request body
             OutputStream reqBody = http.getOutputStream();
 
@@ -46,14 +45,10 @@ public class ServerProxy {
             // request is complete
             reqBody.close();
 
-            Gson gson = new Gson();
-            LoginRequest loginRequest;
-
             // By the time we get here, the HTTP response has been received from the server.
             // Check to make sure that the HTTP response from the server contains a 200
             // status code, which means "success".  Treat anything else as a failure.
             if (http.getResponseCode() == HttpURLConnection.HTTP_OK) {
-
                 // Get the input stream containing the HTTP response body
                 InputStream respBody = http.getInputStream();
 
@@ -88,7 +83,7 @@ public class ServerProxy {
         return null;
     }
 
-    private static void getPeople(String serverHost, String serverPort, String authToken) {
+    private static PersonResult getPeople(String serverHost, String serverPort, String authToken) {
 
         try {
             URL url = new URL("http://" + serverHost + ":" + serverPort + "/person");
@@ -99,17 +94,25 @@ public class ServerProxy {
 
             http.setDoOutput(false);
 
-            http.addRequestProperty("Authorization", authToken); // TODO : What do I put instead of this hardcoded authToken?
+            http.addRequestProperty("Authorization", authToken);
 
             http.addRequestProperty("Accept", "application/json");
 
             http.connect();
 
+            Gson gson = new Gson();
             if (http.getResponseCode() == HttpURLConnection.HTTP_OK) {
-                System.out.println("User successfully registered.");
+                // Get the input stream containing the HTTP response body
+                InputStream respBody = http.getInputStream();
+
+                // Extract JSON data from the HTTP response body
+                String respData = readString(respBody);
+                PersonResult result = gson.fromJson(respData, PersonResult.class);
+                // Display the JSON data returned from the server
+                System.out.println(respData);
+                return result;
             }
             else {
-
                 // The HTTP response status code indicates an error
                 // occurred, so print out the message from the HTTP response
                 System.out.println("ERROR: " + http.getResponseMessage());
@@ -119,18 +122,21 @@ public class ServerProxy {
 
                 // Extract data from the HTTP response body
                 String respData = readString(respBody);
+                PersonResult result = gson.fromJson(respData, PersonResult.class);
 
                 // Display the data returned from the server
                 System.out.println(respData);
+                return result;
             }
         }
         catch (IOException e) {
             // An exception was thrown, so display the exception's stack trace
             e.printStackTrace();
         }
+        return null;
     }
 
-    private static void getEvents(String serverHost, String serverPort, String authToken) {
+    private static EventResult getEvents(String serverHost, String serverPort, String authToken) {
 
         try {
             URL url = new URL("http://" + serverHost + ":" + serverPort + "/event");
@@ -141,17 +147,25 @@ public class ServerProxy {
 
             http.setDoOutput(false);
 
-            http.addRequestProperty("Authorization", authToken); // TODO : What do I put instead of this hardcoded authToken?
+            http.addRequestProperty("Authorization", authToken);
 
             http.addRequestProperty("Accept", "application/json");
 
             http.connect();
 
+            Gson gson = new Gson();
             if (http.getResponseCode() == HttpURLConnection.HTTP_OK) {
-                System.out.println("User successfully registered.");
+                // Get the input stream containing the HTTP response body
+                InputStream respBody = http.getInputStream();
+
+                // Extract JSON data from the HTTP response body
+                String respData = readString(respBody);
+                EventResult result = gson.fromJson(respData, EventResult.class);
+                // Display the JSON data returned from the server
+                System.out.println(respData);
+                return result;
             }
             else {
-
                 // The HTTP response status code indicates an error
                 // occurred, so print out the message from the HTTP response
                 System.out.println("ERROR: " + http.getResponseMessage());
@@ -161,18 +175,21 @@ public class ServerProxy {
 
                 // Extract data from the HTTP response body
                 String respData = readString(respBody);
+                EventResult result = gson.fromJson(respData, EventResult.class);
 
                 // Display the data returned from the server
                 System.out.println(respData);
+                return result;
             }
         }
         catch (IOException e) {
             // An exception was thrown, so display the exception's stack trace
             e.printStackTrace();
         }
+        return null;
     }
 
-    private static void register(String serverHost, String serverPort) {
+    private static RegisterResult register(String serverHost, String serverPort, RegisterRequest r) {
         try {
             URL url = new URL("http://" + serverHost + ":" + serverPort + "/user/register");
 
@@ -186,19 +203,24 @@ public class ServerProxy {
 
             http.connect();
 
-            String reqData = http.getRequestMethod(); // TODO : Is this correct ? (lines 60-69)
+            Gson gson = new Gson();
+            String reqData = gson.toJson(r);
             // Get the output stream containing the HTTP request body
             OutputStream reqBody = http.getOutputStream();
 
             // Write the JSON data to the request body
             writeString(reqData, reqBody);
 
-            // Close the request body output stream, indicating that the
-            // request is complete
-            reqBody.close();
-
             if (http.getResponseCode() == HttpURLConnection.HTTP_OK) {
-                System.out.println("User successfully registered.");
+                // Get the input stream containing the HTTP response body
+                InputStream respBody = http.getInputStream();
+
+                // Extract JSON data from the HTTP response body
+                String respData = readString(respBody);
+                RegisterResult result = gson.fromJson(respData, RegisterResult.class);
+                // Display the JSON data returned from the server
+                System.out.println(respData);
+                return result;
             }
             else {
 
@@ -211,15 +233,18 @@ public class ServerProxy {
 
                 // Extract data from the HTTP response body
                 String respData = readString(respBody);
+                RegisterResult result = gson.fromJson(respData, RegisterResult.class);
 
                 // Display the data returned from the server
                 System.out.println(respData);
+                return result;
             }
         }
         catch (IOException e) {
             // An exception was thrown, so display the exception's stack trace
             e.printStackTrace();
         }
+        return null;
     }
 
 
